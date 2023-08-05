29addcee5446a88efeca744f92bbf7f3eca24c71
mephaline <suaramos88@gmail.com>
2023-08-05 01:02:35 -0500

    creates index.js

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Write your solution here!
+let cats = ['Milo','Otis','Garfield"']
+
+function destructiveAppendCat(name) {
+    cats.push (name);
+}
+//
+function destructivelyPprependCat(name) {
+    cats.unshift(name);
+}
+//
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+//
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+//
+function appendCat(name) {
+    return [...cats, name];
+}
+//
+function appendCat(name) {
+    return [name, ...cats];
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+
+beforeEach(function() {
+    cats = ['Milo', 'Otis', 'Garfield'];
+});
+
+
+function appendCat(name){
+    return [...cats, name];
+
+}
+
+function prependCat(name){
+    return[name, ...cats];
+
+}
+
+
+cats = prependCat(cats);
+console.log(cats);
+
+
+cats.pop();
+console.log(cats);
